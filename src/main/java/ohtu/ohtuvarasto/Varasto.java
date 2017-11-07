74190c5eca16bb6bc2fbc4914bb1f525580c71f4
ollisami <ollisami@ad.helsinki.fi>
2017-11-07 13:52:59 +0200

    Stylecheckin lisays.

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,6 +1,7 @@
 package ohtu.ohtuvarasto;
 
-public class Varasto {
+public class Varasto
+{
 
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
@@ -18,19 +19,16 @@ public class Varasto {
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
+        this.tilavuus = Math.max(tilavuus, 0.0);
+
+        this.saldo = tilavuus; 
+        if (alkuSaldo < 0.0) 
         {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
+        }
+        if (alkuSaldo <= tilavuus) // mahtuu
         {
             this.saldo = alkuSaldo;
-        } else {
-            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
         }
     }
 
